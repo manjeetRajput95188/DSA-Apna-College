90d5ade060543b5895c96c548937fc9807e5ec67
John Sina <johnsina@gmail.com>
2025-02-21 08:52:56 +0530

    Prime Number

---
+++ primeNumber.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+void primeNumber(int n){
+    if(n <= 2){
+        cout<<"Please, enter a valid Number"<<endl;
+    }
+    else{
+     for(int i =1 ; i<+42; i++){ //for(int i =1 ; i*i<+42; i++){
+        if(n % i == 0){
+            cout<<i<<endl;
+        }
+        else{
+            continue;
+        }
+    }
+}
+}
+int main() {
+    int num = 0;
+    cout<<"Enter a number"<<endl;
+    cin>>num;
+    primeNumber(num);
+    
+   
+    return 0;
+}
